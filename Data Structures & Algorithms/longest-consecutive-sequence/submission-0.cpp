e349d1653f9b5a7d4f4b8eae37491fd9140bad5c
Manav Goel <me.manavgoel@gmail.com>
2026-06-29 12:35:31 +0530

    Add: longest-consecutive-sequence - submission-0

---
+++ Data Structures & Algorithms/longest-consecutive-sequence/submission-0.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int longestConsecutive(vector<int>& nums) {
+        unordered_set<int>st;
+        for(auto& it : nums){
+            st.insert(it);
+        }
+        int ans =0;
+        for(auto ele : st){
+            if(st.count(ele-1) == 0){
+                int cnt = 1;
+                int x = ele+1;
+                while(st.count(x)){
+                    cnt++;
+                    x++;
+                }
+                ans = max(ans,cnt);
+            }
+        }
+        return ans;
+    }
+};
